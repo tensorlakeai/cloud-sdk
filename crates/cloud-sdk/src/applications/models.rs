e88e76fc08ccd1c496152e6d17d12cdc8ee34dba
David Calavera <1050+calavera@users.noreply.github.com>
2025-10-29 14:06:41 -0700

    Cleanup field attributes.

--- crates/cloud-sdk/src/applications/models.rs
+++ crates/cloud-sdk/src/applications/models.rs
@@ -6,21 +6,15 @@ use std::collections::HashMap;
 
 #[derive(Clone, Default, Debug, PartialEq, Serialize, Deserialize, Builder)]
 pub struct ApplicationManifest {
-    #[serde(rename = "name")]
     #[builder(setter(into))]
     pub name: String,
-    #[serde(rename = "description")]
     #[builder(setter(into))]
     pub description: String,
-    #[serde(rename = "tags")]
     #[builder(setter(into))]
     pub tags: HashMap<String, String>,
-    #[serde(rename = "version")]
     #[builder(setter(into))]
     pub version: String,
-    #[serde(rename = "functions")]
     pub functions: HashMap<String, FunctionManifest>,
-    #[serde(rename = "entrypoint")]
     pub entrypoint: Entrypoint,
 }
 
@@ -33,16 +27,12 @@ impl ApplicationManifest {
 #[derive(Clone, Default, Debug, PartialEq, Serialize, Deserialize, Builder)]
 #[builder(name = "EntrypointBuilder")]
 pub struct Entrypoint {
-    #[serde(rename = "function_name")]
     #[builder(setter(into))]
     pub function_name: String,
-    #[serde(rename = "input_serializer")]
     #[builder(setter(into))]
     pub input_serializer: String,
-    #[serde(rename = "output_serializer")]
     #[builder(setter(into))]
     pub output_serializer: String,
-    #[serde(rename = "output_type_hints_base64")]
     #[builder(setter(into, strip_option), default)]
     pub output_type_hints_base64: Option<String>,
 }
@@ -56,36 +46,23 @@ impl Entrypoint {
 #[derive(Clone, Default, Debug, PartialEq, Serialize, Deserialize, Builder)]
 #[builder(name = "FunctionManifestBuilder")]
 pub struct FunctionManifest {
-    #[serde(rename = "name")]
     #[builder(setter(into))]
     pub name: String,
-    #[serde(rename = "description")]
     #[builder(setter(into))]
     pub description: String,
-    #[serde(rename = "is_api")]
     pub is_api: bool,
-    #[serde(rename = "secret_names")]
     #[builder(setter(into, strip_option), default)]
     pub secret_names: Vec<String>,
-    #[serde(rename = "initialization_timeout_sec")]
     pub initialization_timeout_sec: i32,
-    #[serde(rename = "timeout_sec")]
     pub timeout_sec: i32,
-    #[serde(rename = "resources")]
     pub resources: Resources,
-    #[serde(rename = "retry_policy")]
     pub retry_policy: RetryPolicy,
-    #[serde(rename = "cache_key", skip_serializing_if = "Option::is_none")]
     #[builder(setter(into, strip_option), default)]
     pub cache_key: Option<String>,
-    #[serde(rename = "parameters")]
     #[builder(setter(into), default)]
     pub parameters: Vec<Parameter>,
-    #[serde(rename = "return_type")]
     pub return_type: serde_json::Value,
-    #[serde(rename = "placement_constraints")]
     pub placement_constraints: PlacementConstraintsManifest,
-    #[serde(rename = "max_concurrency")]
     pub max_concurrency: i32,
 }
 
@@ -98,13 +75,9 @@ impl FunctionManifest {
 #[derive(Clone, Default, Debug, PartialEq, Serialize, Deserialize, Builder)]
 #[builder(name = "ResourcesBuilder")]
 pub struct Resources {
-    #[serde(rename = "cpus")]
     pub cpus: f64,
-    #[serde(rename = "memory_mb")]
     pub memory_mb: i64,
-    #[serde(rename = "ephemeral_disk_mb")]
     pub ephemeral_disk_mb: i64,
-    #[serde(rename = "gpus")]
     #[builder(setter(into), default)]
     pub gpus: Vec<String>,
 }
@@ -118,13 +91,9 @@ impl Resources {
 #[derive(Clone, Default, Debug, PartialEq, Serialize, Deserialize, Builder)]
 #[builder(name = "RetryPolicyBuilder")]
 pub struct RetryPolicy {
-    #[serde(rename = "max_retries")]
     pub max_retries: i32,
-    #[serde(rename = "initial_delay_sec")]
     pub initial_delay_sec: f64,
-    #[serde(rename = "max_delay_sec")]
     pub max_delay_sec: f64,
-    #[serde(rename = "delay_multiplier")]
     pub delay_multiplier: f64,
 }
 
@@ -137,7 +106,6 @@ impl RetryPolicy {
 #[derive(Clone, Default, Debug, PartialEq, Serialize, Deserialize, Builder)]
 #[builder(name = "PlacementConstraintsManifestBuilder")]
 pub struct PlacementConstraintsManifest {
-    #[serde(rename = "filter_expressions")]
     #[builder(setter(into), default)]
     pub filter_expressions: Vec<String>,
 }
@@ -151,7 +119,6 @@ impl PlacementConstraintsManifest {
 #[derive(Clone, Default, Debug, PartialEq, Serialize, Deserialize, Builder)]
 #[builder(name = "ParameterBuilder")]
 pub struct Parameter {
-    #[serde(rename = "name")]
     #[builder(setter(into))]
     pub name: String,
     #[serde(rename = "type")]
@@ -167,107 +134,73 @@ impl Parameter {
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct Allocation {
-    #[serde(rename = "attempt_number")]
     pub attempt_number: i32,
-    #[serde(rename = "created_at")]
     pub created_at: i32,
-    #[serde(
-        rename = "execution_duration_ms",
-        default,
-        skip_serializing_if = "Option::is_none"
-    )]
+    #[serde(default, skip_serializing_if = "Option::is_none")]
     pub execution_duration_ms: Option<i64>,
-    #[serde(rename = "executor_id")]
     pub executor_id: String,
-    #[serde(rename = "function_executor_id")]
     pub function_executor_id: String,
-    #[serde(rename = "function_name")]
     pub function_name: String,
-    #[serde(rename = "id")]
     pub id: String,
-    #[serde(rename = "outcome")]
     pub outcome: FunctionRunOutcome,
 }
 
 #[derive(Clone, Default, Debug, PartialEq, Serialize, Deserialize)]
 pub struct Application {
-    #[serde(rename = "created_at", skip_serializing_if = "Option::is_none")]
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub created_at: Option<i64>,
-    #[serde(rename = "description")]
     pub description: String,
-    #[serde(rename = "entrypoint")]
     pub entrypoint: Box<EntryPointManifest>,
-    #[serde(rename = "functions")]
     pub functions: HashMap<String, ApplicationFunction>,
-    #[serde(rename = "name")]
     pub name: String,
-    #[serde(rename = "namespace")]
     pub namespace: String,
-    #[serde(rename = "tags")]
     pub tags: HashMap<String, String>,
-    #[serde(rename = "tombstoned", skip_serializing_if = "Option::is_none")]
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub tombstoned: Option<bool>,
-    #[serde(rename = "version")]
     pub version: String,
 }
 
 #[derive(Clone, Default, Debug, PartialEq, Serialize, Deserialize)]
 pub struct ApplicationFunction {
-    #[serde(rename = "cache_key", skip_serializing_if = "Option::is_none")]
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub cache_key: Option<String>,
-    #[serde(rename = "description")]
     pub description: String,
-    #[serde(
-        rename = "initialization_timeout_sec",
-        skip_serializing_if = "Option::is_none"
-    )]
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub initialization_timeout_sec: Option<i32>,
-    #[serde(rename = "max_concurrency")]
     pub max_concurrency: i32,
-    #[serde(rename = "name")]
     pub name: String,
-    #[serde(rename = "parameters", skip_serializing_if = "Option::is_none")]
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub parameters: Option<Vec<ParameterMetadata>>,
-    #[serde(rename = "placement_constraints")]
     pub placement_constraints: Box<PlacementConstraints>,
-    #[serde(rename = "resources")]
     pub resources: Box<FunctionResources>,
-    #[serde(rename = "retry_policy")]
     pub retry_policy: Box<NodeRetryPolicy>,
     #[serde(
-        rename = "return_type",
         default,
         with = "::serde_with::rust::double_option",
         skip_serializing_if = "Option::is_none"
     )]
     pub return_type: Option<Option<serde_json::Value>>,
-    #[serde(rename = "secret_names")]
     pub secret_names: Vec<String>,
-    #[serde(rename = "timeout_sec")]
     pub timeout_sec: i32,
 }
 
 #[derive(Clone, Default, Debug, PartialEq, Serialize, Deserialize)]
 pub struct ApplicationRequests {
-    #[serde(rename = "cursor", skip_serializing_if = "Option::is_none")]
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub cursor: Option<String>,
-    #[serde(rename = "requests")]
     pub requests: Vec<ShallowRequest>,
 }
 
 #[derive(Clone, Default, Debug, PartialEq, Serialize, Deserialize)]
 pub struct ApplicationsList {
-    #[serde(rename = "applications")]
     pub applications: Vec<Application>,
-    #[serde(rename = "cursor", skip_serializing_if = "Option::is_none")]
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub cursor: Option<String>,
 }
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Ord, PartialOrd, Hash, Serialize, Deserialize)]
 pub enum CursorDirection {
-    #[serde(rename = "forward")]
     Forward,
-    #[serde(rename = "backward")]
     Backward,
 }
 
@@ -289,122 +222,93 @@ pub struct DownloadOutput {
 
 #[derive(Clone, Default, Debug, PartialEq, Serialize, Deserialize)]
 pub struct EntryPointManifest {
-    #[serde(rename = "function_name")]
     pub function_name: String,
-    #[serde(rename = "name")]
     pub name: String,
-    #[serde(rename = "version")]
     pub version: String,
 }
 
 #[derive(Clone, Default, Debug, PartialEq, Serialize, Deserialize)]
 pub struct FunctionResources {
-    #[serde(rename = "cpu_count")]
     pub cpu_count: i32,
-    #[serde(rename = "gpus", skip_serializing_if = "Option::is_none")]
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub gpus: Option<Vec<GpuResources>>,
-    #[serde(rename = "memory_bytes")]
     pub memory_bytes: i64,
-    #[serde(rename = "storage_bytes", skip_serializing_if = "Option::is_none")]
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub storage_bytes: Option<i64>,
 }
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct FunctionRun {
-    #[serde(rename = "allocations")]
     pub allocations: Vec<Allocation>,
-    #[serde(rename = "created_at")]
     pub created_at: i64,
     #[serde(rename = "id")]
     pub id: String,
-    #[serde(rename = "outcome", skip_serializing_if = "Option::is_none")]
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub outcome: Option<Option<FunctionRunOutcome>>,
-    #[serde(rename = "status")]
     pub status: FunctionRunStatus,
 }
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Ord, PartialOrd, Hash, Serialize, Deserialize)]
 pub enum FunctionRunOutcome {
-    #[serde(rename = "success")]
     Success,
-    #[serde(rename = "failure")]
     Failure,
 }
 
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Ord, PartialOrd, Hash, Serialize, Deserialize)]
 pub enum FunctionRunStatus {
-    #[serde(rename = "pending")]
     Pending,
-    #[serde(rename = "enqueued")]
     Enqueued,
-    #[serde(rename = "running")]
     Running,
-    #[serde(rename = "completed")]
     Completed,
-    #[serde(rename = "failed")]
     Failed,
 }
 
 #[derive(Clone, Default, Debug, PartialEq, Serialize, Deserialize)]
 pub struct GpuResources {
-    #[serde(rename = "count")]
     pub count: i32,
-    #[serde(rename = "model")]
     pub model: String,
 }
 
 #[derive(Clone, Default, Debug, PartialEq, Serialize, Deserialize)]
 pub struct NodeRetryPolicy {
-    #[serde(rename = "backoff_multiplier")]
     pub backoff_multiplier: f64,
-    #[serde(rename = "delay_multiplier", skip_serializing_if = "Option::is_none")]
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub delay_multiplier: Option<f64>,
-    #[serde(rename = "max_retries")]
     pub max_retries: i32,
-    #[serde(rename = "timeout_secs")]
     pub timeout_secs: i64,
 }
 
 #[derive(Clone, Default, Debug, PartialEq, Serialize, Deserialize)]
 pub struct ParameterMetadata {
-    #[serde(rename = "data_type")]
     pub data_type: String,
-    #[serde(rename = "default_value", skip_serializing_if = "Option::is_none")]
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub default_value: Option<String>,
-    #[serde(rename = "description", skip_serializing_if = "Option::is_none")]
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub description: Option<String>,
-    #[serde(rename = "name")]
     pub name: String,
-    #[serde(rename = "required")]
     pub required: bool,
 }
 
 #[derive(Clone, Default, Debug, PartialEq, Serialize, Deserialize)]
 pub struct PlacementConstraints {
     /// List of label filter expressions in the format "key=value", "key!=value", etc.
-    #[serde(rename = "locations", skip_serializing_if = "Option::is_none")]
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub locations: Option<Vec<String>>,
 }
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct Request {
-    #[serde(rename = "created_at")]
     pub created_at: i64,
-    #[serde(rename = "function_runs")]
     pub function_runs: Vec<FunctionRun>,
-    #[serde(rename = "id")]
     pub id: String,
-    #[serde(rename = "outcome")]
     pub outcome: RequestOutcome,
-    #[serde(rename = "request_error", skip_serializing_if = "Option::is_none")]
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub request_error: Option<Option<Box<RequestError>>>,
 }
 
 #[derive(Clone, Default, Debug, PartialEq, Serialize, Deserialize)]
 pub struct RequestError {
-    #[serde(rename = "function_name")]
     pub function_name: String,
-    #[serde(rename = "message")]
     pub message: String,
 }
 
@@ -423,13 +327,11 @@ pub enum RequestOutcome {
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct RequestOutcomeOneOf {
-    #[serde(rename = "failure")]
     pub failure: RequestFailureReason,
 }
 
 #[derive(Clone, Default, Debug, PartialEq, Serialize, Deserialize)]
 pub struct ShallowRequest {
-    #[serde(rename = "created_at")]
     pub created_at: i64,
     #[serde(rename = "id")]
     pub id: String,
--- crates/cloud-sdk/src/secrets/models.rs
+++ crates/cloud-sdk/src/secrets/models.rs
@@ -3,9 +3,7 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct Secret {
-    #[serde(rename = "id")]
     pub id: String,
-    #[serde(rename = "name")]
     pub name: String,
     #[serde(rename = "createdAt")]
     pub created_at: String,
@@ -13,9 +11,7 @@ pub struct Secret {
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct CreateSecret {
-    #[serde(rename = "name")]
     pub name: String,
-    #[serde(rename = "value")]
     pub value: String,
 }
 
@@ -74,19 +70,16 @@ pub enum UpsertSecretResponse {
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct SecretsList {
-    #[serde(rename = "items")]
     pub items: Vec<Secret>,
-    #[serde(rename = "pagination")]
     pub pagination: Pagination,
 }
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct Pagination {
-    #[serde(rename = "next", skip_serializing_if = "Option::is_none")]
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub next: Option<String>,
-    #[serde(rename = "prev", skip_serializing_if = "Option::is_none")]
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub prev: Option<String>,
-    #[serde(rename = "total")]
     pub total: i32,
 }
 
